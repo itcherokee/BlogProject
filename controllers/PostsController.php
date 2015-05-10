b9c3f56c3f79b9ee8b5b6c38cfad31efc4be446c
itcherokee <simeonov23@yahoo.com>
2015-05-10 13:36:35 +0300

    fixed denying requests to Home and Admin blogs

--- controllers/PostsController.php
+++ controllers/PostsController.php
@@ -20,93 +20,98 @@ class PostsController extends BaseController
 
     public function index($id = array())
     {
-        // loads all posts historically by year, by month, by day
-        $this->historyList = $this->modelData->getPostsHistorically($this->blogName);
+        if($this->blogName != SYSTEM_BLOG && $this->blogName != ADMIN_BLOG){
+            // loads all posts historically by year, by month, by day
+            $this->historyList = $this->modelData->getPostsHistorically($this->blogName);
 
-        // loads most popular tags
-        $this->mostPopularTags = $this->modelData->getMostPopularTags($this->blogName);
+            // loads most popular tags
+            $this->mostPopularTags = $this->modelData->getMostPopularTags($this->blogName);
 
-        // check is it single post or multiple posts request
-        if (count($id) > 0) {
-            $this->isSinglePost = true;
-            $this->lastPage = 0;
-            $this->posts = $this->modelData->getPostById($id[0]);
-            $this->modelData->increasePostView($this->posts[0]['visits'] + 1, $id[0]);
-        } else {
-            $this->isSinglePost = false;
-
-            // check is it historical posts reqest
-            if (isset($_GET['year']) && isset($_GET['month'])) {
-                $dtime = \DateTime::createFromFormat("Y/m/d", $_GET['year'] . '/' . $_GET['month'] . '/1');
-                $startDateTimestamp = $dtime->getTimestamp();
-                $this->startDate = date("Y-m-d", $startDateTimestamp);
-                $this->endDate = date("Y-m-t", $startDateTimestamp);
-                $this->historyPeriod = 'year=' . $_GET['year'] . '&month=' . $_GET['month'];
+            // check is it single post or multiple posts request
+            if (count($id) > 0) {
+                $this->isSinglePost = true;
+                $this->lastPage = 0;
+                $this->posts = $this->modelData->getPostById($id[0]);
+                $this->modelData->increasePostView($this->posts[0]['visits'] + 1, $id[0]);
             } else {
-                $this->historyPeriod = '';
-                $minMaxPosts = $this->modelData->getOldestAndNewestPostDate($this->blogName);
-                if ($minMaxPosts[0]['newest'] != null) {
-                    $dtime = \DateTime::createFromFormat("Y-m-d", $minMaxPosts[0]['oldest']);
+                $this->isSinglePost = false;
+
+                // check is it historical posts reqest
+                if (isset($_GET['year']) && isset($_GET['month'])) {
+                    $dtime = \DateTime::createFromFormat("Y/m/d", $_GET['year'] . '/' . $_GET['month'] . '/1');
                     $startDateTimestamp = $dtime->getTimestamp();
                     $this->startDate = date("Y-m-d", $startDateTimestamp);
-                    $dtime = \DateTime::createFromFormat("Y-m-d", $minMaxPosts[0]['newest']);
-                    $startDateTimestamp = $dtime->getTimestamp();
                     $this->endDate = date("Y-m-t", $startDateTimestamp);
+                    $this->historyPeriod = 'year=' . $_GET['year'] . '&month=' . $_GET['month'];
                 } else {
-                    $currentDate = time();
-                    $this->startDate = date("Y-m-d", $currentDate);
-                    $this->endDate = date("Y-m-t", $currentDate);
+                    $this->historyPeriod = '';
+                    $minMaxPosts = $this->modelData->getOldestAndNewestPostDate($this->blogName);
+                    if ($minMaxPosts[0]['newest'] != null) {
+                        $dtime = \DateTime::createFromFormat("Y-m-d", $minMaxPosts[0]['oldest']);
+                        $startDateTimestamp = $dtime->getTimestamp();
+                        $this->startDate = date("Y-m-d", $startDateTimestamp);
+                        $dtime = \DateTime::createFromFormat("Y-m-d", $minMaxPosts[0]['newest']);
+                        $startDateTimestamp = $dtime->getTimestamp();
+                        $this->endDate = date("Y-m-t", $startDateTimestamp);
+                    } else {
+                        $currentDate = time();
+                        $this->startDate = date("Y-m-d", $currentDate);
+                        $this->endDate = date("Y-m-t", $currentDate);
+                    }
                 }
+
+                $this->lastPage = (int)floor($this->modelData->countAllPostsPerBlog($this->blogName, $this->startDate, $this->endDate) / $this->pageSize);
+
+                // calculate navigation
+                $this->currentPage = 0;
+                if (isset($_GET['page'])) {
+                    $this->currentPage = $_GET['page'];
+                }
+
+                if ($this->currentPage < $this->firstPage) {
+                    $this->currentPage = $this->firstPage;
+                }
+
+                if ($this->currentPage > $this->lastPage) {
+                    $this->currentPage = $this->lastPage;
+                }
+
+                $from = $this->currentPage * $this->pageSize;
+                $this->posts = $this->modelData->getPostsPerBlogWithLimitByDate($this->blogName, $this->startDate, $this->endDate, $from, $this->pageSize);
             }
 
-            $this->lastPage = (int)floor($this->modelData->countAllPostsPerBlog($this->blogName, $this->startDate, $this->endDate) / $this->pageSize);
+            foreach ($this->posts as $key => $post) {
+                if ($this->isSinglePost) {
+                    $post['visits'] = $post['visits'] + 1;
+                    $comments = $this->modelData->getAllCommentsPerPost($post['id']);
+                    if (count($comments) > 0) {
+                        $post['comments'] = $comments;
+                    }
+                } else {
+                    $post['title'] = mb_substr($post['title'], 0, 70) . '...';
+                    $post['text'] = mb_substr($post['text'], 0, 100) . '...';
+                }
 
-            // calculate navigation
-            $this->currentPage = 0;
-            if (isset($_GET['page'])) {
-                $this->currentPage = $_GET['page'];
+                $tags = $this->modelData->getAllTagsPerPost($post['id']);
+                if (count($tags) > 0) {
+                    $combinedTags = array();
+                    foreach ($tags as $tag) {
+                        $combinedTags[] = $tag['name'];
+                    }
+
+                    $tags = implode(', ', $combinedTags);
+                    $post['tags'] = $tags;
+                }
+
+
+                $this->posts[$key] = $post;
             }
 
-            if ($this->currentPage < $this->firstPage) {
-                $this->currentPage = $this->firstPage;
-            }
-
-            if ($this->currentPage > $this->lastPage) {
-                $this->currentPage = $this->lastPage;
-            }
-
-            $from = $this->currentPage * $this->pageSize;
-            $this->posts = $this->modelData->getPostsPerBlogWithLimitByDate($this->blogName, $this->startDate, $this->endDate, $from, $this->pageSize);
+            $this->renderView();
         }
-
-        foreach ($this->posts as $key => $post) {
-            if ($this->isSinglePost) {
-                $post['visits'] = $post['visits'] + 1;
-                $comments = $this->modelData->getAllCommentsPerPost($post['id']);
-                if (count($comments) > 0) {
-                    $post['comments'] = $comments;
-                }
-            } else {
-                $post['title'] = mb_substr($post['title'], 0, 70) . '...';
-                $post['text'] = mb_substr($post['text'], 0, 100) . '...';
-            }
-
-            $tags = $this->modelData->getAllTagsPerPost($post['id']);
-            if (count($tags) > 0) {
-                $combinedTags = array();
-                foreach ($tags as $tag) {
-                    $combinedTags[] = $tag['name'];
-                }
-
-                $tags = implode(', ', $combinedTags);
-                $post['tags'] = $tags;
-            }
-
-
-            $this->posts[$key] = $post;
+        else {
+            $this->redirect($this->blogName, DEFAULT_CONTROLLER, DEFAULT_ACTION);
         }
-
-        $this->renderView();
     }
 
     public function create()
